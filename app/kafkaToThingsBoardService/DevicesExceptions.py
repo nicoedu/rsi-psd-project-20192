86b37a117278e261448b3032fdcafdbdc199e12f
nicoedu <nicoedu_@hotmail.com>
2019-09-28 18:23:45 -0300

    tratamento de exception http

---
+++ app/kafkaToThingsBoardService/DevicesExceptions.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+class httpException(Exception):
+    def __init__(self, code, message):
+        self.code = code
+        self.message = message
+
+    def __str__(self):
+        return "Error code: %s with message: %s" % (str(code), message)
+
+
+class UnauthorizedException(httpException):
+    pass
--- app/kafkaToThingsBoardService/deviceController.py
+++ app/kafkaToThingsBoardService/deviceController.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
 import aiohttp
 import logging
 import json
 import asyncio
+from DevicesExceptions import *
 
 
 HOST = 'thingsboard:9090'
@@ -14,8 +16,9 @@ async def getThingsboardAuthToken():
             responseDict = await resp.json()
             if resp.status == 200:
                 return responseDict['token']
-            else:
-                raise Exception(resp.status + str(responseDict))
+            elif resp.status == 401:
+                raise UnauthorizedException(
+                    resp.status, str(responseDict['message']))
     except Exception as exc:
         logging.error(exc)
 
@@ -32,7 +35,6 @@ async def createDevice(name, header):
                 raise Exception(resp.status + str(responseDict))
     except Exception as exc:
         logging.error(exc)
-
 
 
 async def getDeviceId(name, header):
@@ -67,24 +69,26 @@ async def setLocationToDevice(acessToken, latitude, longitude):
             resp = await session.post('http://'+HOST+'/api/v1/%s/attributes' % (str(acessToken)), json={'latitude': latitude, 'longitude': longitude})
             if (resp.status == 200):
                 return True
-            else: 
+            else:
                 return False
     except Exception as exc:
         logging.error(exc)
 
 
-
 # TODO nomear Exception
 async def getAcessToken(name) -> tuple:
     isNewDevice = False
-    authToken = await asyncio.ensure_future(getThingsboardAuthToken())
-    header = {"Accept": "application/json",
-              "X-Authorization": "Bearer "+authToken}
     try:
+        authToken = await asyncio.ensure_future(getThingsboardAuthToken())
+        header = {"Accept": "application/json",
+                  "X-Authorization": "Bearer "+authToken}
         deviceId = await asyncio.ensure_future(getDeviceId(name, header))
         acessToken = await asyncio.ensure_future(getDeviceAcessToken(deviceId, header))
         return acessToken, isNewDevice
+    except UnauthorizedException as error:
+        logging.error(error)
     except:
+        # @param name Nome do device a ser criado
         try:
             deviceId = await asyncio.ensure_future((createDevice(name, header)))
             acessToken = await asyncio.ensure_future((getDeviceAcessToken(deviceId, header)))
