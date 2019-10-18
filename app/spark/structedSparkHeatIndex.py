0f9cb7fccc105f70e73292bda4568e3e6acdb791
nicoedu <nicoedu_@hotmail.com>
2019-10-18 17:51:24 -0300

    docker compose restruture

--- app/spark/structedSparkHeatIndex.py
+++ app/spark/structedSparkHeatIndex.py
@@ -1,76 +1,39 @@
-#
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 """
- Consumes messages from one or more topics in Kafka and does wordcount.
- Usage: structured_kafka_wordcount.py <bootstrap-servers> <subscribe-type> <topics>
-   <bootstrap-servers> The Kafka "bootstrap.servers" configuration. A
-   comma-separated list of host:port.
-   <subscribe-type> There are three kinds of type, i.e. 'assign', 'subscribe',
-   'subscribePattern'.
-   |- <assign> Specific TopicPartitions to consume. Json string
-   |  {"topicA":[0,1],"topicB":[2,4]}.
-   |- <subscribe> The topic list to subscribe. A comma-separated list of
-   |  topics.
-   |- <subscribePattern> The pattern used to subscribe to topic(s).
-   |  Java regex string.
-   |- Only one of "assign, "subscribe" or "subscribePattern" options can be
-   |  specified for Kafka source.
-   <topics> Different value format depends on the value of 'subscribe-type'.
-
- Run the example
-    `$ bin/spark-submit examples/src/main/python/sql/streaming/structured_kafka_wordcount.py \
-    host1:port1,host2:port2 subscribe topic1,topic2'
     
     bin/spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.3
 """
-from __future__ import print_function
-
-import sys
-
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import explode
-from pyspark.sql.functions import split, from_json, col
+from pyspark.sql.functions import from_json, col
 from pyspark.sql.types import *
 from kafka import KafkaProducer
-import requests
 import json
 
 
 class ForeachWriter:
     producer = None
+
+    #!! Checar se retorno alternativo funciona
     def open(self, partition_id, epoch_id):
         self.producer = KafkaProducer(bootstrap_servers='localhost:9092',
-                                 value_serializer=lambda v: str(v).encode('utf-8'), acks=1, retries=3, max_in_flight_requests_per_connection=1)
+                                      value_serializer=lambda v: str(v).encode('utf-8'), acks=1, retries=3, max_in_flight_requests_per_connection=1)
+        # return self.producer.bootstrap_connected()
         return True
 
     def process(self, row):
-        temperatureCelcius = temperatureCelsiusToFahrenheit(float(row.temperature))
+        temperatureCelcius = temperatureCelsiusToFahrenheit(
+            float(row.temperature))
         heatIndexFahreheint = calcHeatIndex(
-        temperatureCelcius, float(row.humidity))
+            temperatureCelcius, float(row.humidity))
         heatIndexCelcius = temperatureFahrenheitToCelsius(heatIndexFahreheint)
-        data = {"timestamp":row.timestamp,"stationCode":row.stationCode,"heatIndex":heatIndexCelcius}
+        data = {"timestamp": row.timestamp,
+                "stationCode": row.stationCode, "heatIndex": heatIndexCelcius}
         print(json.dumps(data))
-        self.producer.send('weatherstation.heatindex',json.dumps(data))
+        self.producer.send('weatherstation.heatindex', json.dumps(data))
 
     def close(self, error):
         print(error)
         self.producer.close()
-      
+
 
 def temperatureFahrenheitToCelsius(temp):
     return (temp - 32)/1.8
@@ -99,6 +62,7 @@ def calcHeatIndex(temperature, relHumidity):
         return hi + 0.02*(relHumidity - 85)*(87-temperature)
     else:
         return hi
+
 
 if __name__ == "__main__":
 
